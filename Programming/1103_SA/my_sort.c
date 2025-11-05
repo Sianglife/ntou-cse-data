b2f3be0d3a32de5cb04f4e7418dbb52053d1900c
Siang Huang <sianglifen@gmail.com>
2025-11-05 15:38:20 +0800

    fix: bubble sort

--- Programming/1103_SA/my_sort.c
+++ Programming/1103_SA/my_sort.c
@@ -91,7 +91,7 @@ void bubbleSort(int arr[], int n) {
     /* 兩兩比較、較大的往後移或較小
     的往前移，像氣泡一樣浮上去。 */
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n - i; j++) {
+        for(int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j+1]) {
                 swap(&arr[j], &arr[j+1]);
             }
