bcbf5f2d9fb50cce9166c0f4678e7030e63559f9
Siang Huang <sianglifen@gmail.com>
2025-11-10 20:46:22 +0800

    add: bangla numbers

--- _CPE_Practice/10101_Bangla_Numbers.cpp
+++ _CPE_Practice/10101_Bangla_Numbers.cpp
@@ -9,19 +9,38 @@
 #define n1 100
 using namespace std;
 
+// TODO: 拿99999999999999試，會發現輸出>99，要再處理
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, mod, tmp;
+    int number_base[4] = {n4, n3, n2, n1};
+    string text_base[4] = {t4, t3, t2, t1};
+    int cnt=0;
+    long long int n, mod, tmp;
     while(cin >> n) {
-        cout << "ABC";
-        tmp = n / n4;
-        cout << tmp << "ABC";
-        if (tmp != 0) {
-            cout << tmp << " " << t4;
+        cnt++;
+        cout << "   " << cnt << ".";
+        for(int i=0; i<4;) {
+            while (n >= number_base[i]) {
+                tmp = n / number_base[i];
+                if (tmp <= 99) {
+                    n = n % number_base[i];
+                    cout << " " << tmp << " " << text_base[i];
+                } else {
+                    while (n > 99) {
+                        n = n % number_base[i];
+                        cout << " " << tmp << " " << text_base[i];
+
+                    }
+                } 
+            }
+            i++;
+            continue;
+        }
+        if (n != 0) {
+            cout << " " << n << "\n";
+            return 0;
         }
     }
-
-
 }
