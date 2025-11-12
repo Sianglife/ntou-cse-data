fb3bdf67f29891905918c338346161d07e0a2550
Siang Huang <sianglifen@gmail.com>
2025-11-12 11:56:16 +0800

    add: uva 10101

--- _CPE_Practice/10101_Bangla_Numbers.cpp
+++ _CPE_Practice/10101_Bangla_Numbers.cpp
@@ -9,38 +9,45 @@
 #define n1 100
 using namespace std;
 
-// TODO: 拿99999999999999試，會發現輸出>99，要再處理
+int number_base[4] = {n4, n3, n2, n1};
+string text_base[4] = {t4, t3, t2, t1};
+
+void transfer_output(long long int n) {
+    long long int tmp;
+    if (n == 0) {
+        cout << " " << n;
+    }
+    for(int i=0; i<4;) {
+        while (n >= number_base[i]) {
+            // cout << "\n'" << n << "'\n";
+            tmp = n / number_base[i];
+            if (tmp > 99) {
+                transfer_output(tmp);                
+                n = n % number_base[i];                
+                cout << " " << text_base[i];
+                continue;
+            }
+            n = n % number_base[i];
+            cout << " " << tmp << " " << text_base[i];
+        }
+        i++;
+        continue;
+    }
+    if (n != 0) {
+        cout << " " << n;
+    }
+}
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int number_base[4] = {n4, n3, n2, n1};
-    string text_base[4] = {t4, t3, t2, t1};
     int cnt=0;
-    long long int n, mod, tmp;
+    long long int n;
     while(cin >> n) {
         cnt++;
         cout << "   " << cnt << ".";
-        for(int i=0; i<4;) {
-            while (n >= number_base[i]) {
-                tmp = n / number_base[i];
-                if (tmp <= 99) {
-                    n = n % number_base[i];
-                    cout << " " << tmp << " " << text_base[i];
-                } else {
-                    while (n > 99) {
-                        n = n % number_base[i];
-                        cout << " " << tmp << " " << text_base[i];
-
-                    }
-                } 
-            }
-            i++;
-            continue;
-        }
-        if (n != 0) {
-            cout << " " << n << "\n";
-            return 0;
-        }
+        transfer_output(n);
+        cout << "\n";
     }
 }
