a122f4862f63797833f0bbd6fabc74340e143953
Siang Huang <sianglifen@gmail.com>
2025-11-17 19:20:20 +0800

    update: programming lab 9

--- Programming/1121_LAB9/Lab_9_1_Classify_Fruit_C.c
+++ Programming/1121_LAB9/Lab_9_1_Classify_Fruit_C.c
@@ -1,17 +1,19 @@
-/*Date: 2025/11/14
+/*Date: 2025/11/17
 Author: 黃鈺翔
-Description: 
+Description: Sorting an array of fruit using bubble sort
 Using github copilot to write comments
 */
 #include<stdio.h>
 
 void swap(int *a, int *b) {
+    // swap the values of two integers
     int tmp = *b;
     *b = *a;
     *a = tmp;
 }
 
 void sortFruits(int arr[], size_t size) {
+    // bubble sort algorithm
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
@@ -24,11 +26,16 @@ void sortFruits(int arr[], size_t size) {
 int main() {
     int n = 0;
     int arr[1000];
+
+    // Input fruit classifications until EOF
     while(scanf("%d", &arr[n]) == 1) {
         n++;
     }
+
+    // Sort the fruit classifications
     sortFruits(arr, n);
 
+    // Output the sorted array
     printf("[");
     for (int i = 0; i < n; i++) {
         if (i != n - 1) {
