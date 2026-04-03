ef11c17f19b2177fc0ae96038d99c92db1100a80
sianglife <sianglifen@gmail.com>
2026-04-04 00:01:59 +0800

    remove UVA

--- Programming/0327/Linked_list_sort.cpp
+++ Programming/0327/Linked_list_sort.cpp
@@ -19,15 +19,73 @@ int Number::getNumber() {
 }  
   
 shared_ptr<Number> head;  
-shared_ptr<Number> tail;  
+shared_ptr<Number> tail;
+
+void moveFront(shared_ptr<Number> &current) {
+    /*
+    1 2 3 4 5
+    1 3 2 4 5
+    3:              
+    pre:2       1
+    next:4      2
+
+    2: 
+    pre: 1      3
+    next: 3     4
+
+    1:
+    pre: null   null
+    next: 2     3
+
+    4:
+    pre: 3      2
+    next: 5     5
+
+    */
+    // current 往前一
+    // current.previous
+}
   
 void sortDLL() {
+    shared_ptr<Number> current;
+    current = head;
+    while (current->next != NULL) {
+        if (current->next->getNumber() < current->getNumber()) {
+            shared_ptr<Number> large, small, tmp;
+            large = current;
+            small = current->next;
+            // Swap Order
+            /*
+            large:
+            p = n
+            n = small.n
+
+            small:
+            p = large.p
+            n = p
+            */
+            tmp = large;
+            tmp->previous = tmp->next;
+            tmp->next = small->next;
+            small->next = small->previous;
+            small->previous = large->previous;
+            large = tmp;            
+        }
+        current = current->next;
+    }
 }  
   
-void printDLLNumber() {  
+void printDLLNumber() {
+    shared_ptr<Number> current;
+    current = head;
+    while (current->next != NULL) {
+        cout << current->getNumber() << " ";
+        current = current->next;
+    }
+    cout << current->getNumber() << " ";
 }  
   
-void readInput(vector & v) {  
+void readInput(vector<int> & v) {  
     string line, token;  
   
     getline(cin, line);  
@@ -39,18 +97,32 @@ void readInput(vector & v) {
     }  
 }  
   
-void makeDLL(vector v) {  
+void makeDLL(vector<int> v) {
+    shared_ptr<Number> current, previous, next;
+    previous = NULL;
+    head = make_shared<Number>(v[0]);
+    current = head;
+    next = NULL;
+
+    for (int i = 1; i < v.size(); i++) {
+        next = make_shared<Number>(v[i]);
+        current->next = next;
+        current->previous = previous;
+
+        previous = current;
+        current = next;
+    }
+    current->next = NULL;
 }  
   
 int main(int argc, char * argv[]) {  
-  
-    vector v;  
+    vector<int> v;  
     readInput(v);  
     if (v.size() == 0) {  
-        cout << endl;  
+        cout << endl;
         return 0;  
     }  
     makeDLL(v);  
-    sortDLL();  
+    sortDLL();
     printDLLNumber();  
 }
